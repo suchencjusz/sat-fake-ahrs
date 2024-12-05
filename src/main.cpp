2b5f558ad63b80641b8f630f31366dd4216636ad
suchencjusz <js316299@student.polsl.pl>
2024-12-05 16:59:45 +0100

    feat: magnetometr & debug

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,22 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <LSM6.h>
+#include <LIS3MDL.h>
 
+LIS3MDL mag;
 LSM6 imu;
+
+// #define DEBUG
+
+#ifdef DEBUG
+unsigned long __data_sent = 0;
+#endif
 
 void device_status(int status)
 {
+  // 1 = imu failed
+  // 2 = mag failed
+
   if (status == 1)
   {
     digitalWrite(LED_BUILTIN, HIGH);
@@ -13,11 +24,25 @@ void device_status(int status)
     digitalWrite(LED_BUILTIN, LOW);
     delay(1000);
   }
+
+  if (status == 2)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(100);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(100);
+  }
 }
 
-void get_imu_data()
+void get_data()
 {
+#ifdef DEBUG
+  __data_sent++;
+#endif
+
   imu.read();
+  mag.read();
+
   Serial.print(imu.g.x);
   Serial.print(",");
   Serial.print(imu.g.y);
@@ -29,6 +54,19 @@ void get_imu_data()
   Serial.print(imu.a.y);
   Serial.print(",");
   Serial.print(imu.a.z);
+  Serial.print(",");
+
+  Serial.print(mag.m.x);
+  Serial.print(",");
+  Serial.print(mag.m.y);
+  Serial.print(",");
+  Serial.print(mag.m.z);
+
+#ifdef DEBUG
+  Serial.print(",");
+  Serial.print(__data_sent);
+#endif
+
   Serial.println();
 }
 
@@ -47,13 +85,22 @@ void setup()
     }
   }
 
+  if (!mag.init())
+  {
+    Serial.println("Failed to detect and initialize magnetometer!");
+    while (1)
+    {
+      device_status(2);
+    }
+  }
+
   imu.enableDefault();
+  mag.enableDefault();
 }
 
 void loop()
 {
-  get_imu_data();
-  delay(100);
+  get_data();
 
   // bajer do resetowania esp jakby cos walnelo (padnie polaczenie z imu przez te smieszne kable)
   if (Serial.available() > 0)
